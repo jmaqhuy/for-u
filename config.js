0a35442c017bd0564e08bd532e688f7340f6bda4
Quoc Huy <quochuy.ws@gmail.com>
2024-10-08 11:15:35 +0700

    initial commit

---
+++ config.js
@@ -0,0 +1,16 @@
+const CONFIG = {
+    introTitle: 'Em à!',
+    introDesc: `Ngàn vạn câu yêu cũng không bằng một chiều anh đưa tay nắm, bảo với em rằng: một đời dài lắm, hãy để anh được che chở em`,
+    btnIntro: 'Bấm vô đây nè',
+    title: 'Phải chăng em cũng đã phải lòng anh rồi',
+    desc: '',
+    btnYes: 'Vâng, anh phải hứa là che chở cho em đấy',
+    btnNo: 'Không, anh nghĩ nhiều rồi!',
+    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh',
+    btnReply: 'Gửi cho anh <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
+    mess: 'Hẹn em vào một chiều thu nhé!',
+    messDesc: 'Chúng ta sẽ lại cùng nhau ăn tối, trò chuyện vui cười với nhau',
+    btnAccept: 'Oki anh nhé!',
+    messLink: 'https://m.me/HuyyBoDoi'
+}
